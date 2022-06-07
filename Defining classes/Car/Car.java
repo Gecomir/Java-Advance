f4e81dfba3f3b7a92bef1ae0017fff21da23d83a
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-06-07 20:30:38 +0300

    Add files via upload

---
+++ Defining classes/Car/Car.java
@@ -0,0 +1,36 @@
+package Defining_classes;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String carInfo() {
+        return String.format("The car is: %s %s - %d HP.", getBrand(), getModel(), getHorsePower());
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+
+    }
+}
