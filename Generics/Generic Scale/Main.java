e1b6a668a8ba2691249b216e1b3984997496a0fb
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-06-13 19:16:21 +0300

    Add files via upload

---
+++ Generics/Generic Scale/Main.java
@@ -0,0 +1,10 @@
+package Generics.Jar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scale<String> scale = new Scale<>("A", "B");
+
+        System.out.println(scale.getHeavier());
+    }
+}
