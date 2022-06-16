7ed270a4a63267be53cc07dd33dcf58bcea84e65
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-06-16 18:10:10 +0300

    Add files via upload

---
+++ Generics/Generic Swap Method Integer/Box.java
@@ -0,0 +1,33 @@
+package Generics.Jar.GenericSwapMethodInteger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Box <T>{
+
+    private List<T> values;
+
+    public Box() {
+
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+
+        values.add(element);
+    }
+
+    public void swap(int firstIndex, int secondIndex) {
+        Collections.swap(values, firstIndex, secondIndex);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (T value : values) {
+            stringBuilder.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+        return stringBuilder.toString();
+    }
+}
