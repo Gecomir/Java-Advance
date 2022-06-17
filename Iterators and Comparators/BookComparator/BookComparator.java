b4d4eeb03eae4218719fc94aa191c1c280a5e4dd
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-06-17 20:00:00 +0300

    Add files via upload

---
+++ Iterators and Comparators/BookComparator/BookComparator.java
@@ -0,0 +1,12 @@
+package IteratorsAndComparators;
+
+import IteratorsAndComparators.Book.Book;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book f, Book s) {
+        return compare(f, s);
+    }
+}
