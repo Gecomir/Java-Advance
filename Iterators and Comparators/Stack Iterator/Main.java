330f521beead68d0e72571e03931a2a81d09df4a
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-06-19 19:20:40 +0300

    Add files via upload

---
+++ Iterators and Comparators/Stack Iterator/Main.java
@@ -0,0 +1,11 @@
+package IteratorsAndComparators.StackIterator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
