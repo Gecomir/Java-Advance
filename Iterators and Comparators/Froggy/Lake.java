0ff2bc6214f90a74dbfece0fb31d9597f74e3417
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-06-20 21:18:06 +0300

    Add files via upload

---
+++ Iterators and Comparators/Froggy/Lake.java
@@ -0,0 +1,38 @@
+package IteratorsAndComparators.Froggy;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Lake implements Iterable<Integer> {
+    private List<Integer> numbers;
+
+    public Lake(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Frog();
+    }
+
+    class Frog implements Iterator<Integer> {
+        private int index = 0;
+        boolean firstRoundFinish = false;
+
+        @Override
+        public boolean hasNext() {
+            return index < numbers.size();
+        }
+
+        @Override
+        public Integer next() {
+            int currentINdex = index;
+            index += 2;
+            if (index >= numbers.size() && !firstRoundFinish) {
+                index = 1;
+                firstRoundFinish = true;
+            }
+            return numbers.get(currentINdex);
+        }
+    }
+}
