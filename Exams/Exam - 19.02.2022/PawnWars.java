41d4f727a2ea1f54f9b45d4418a2899626ec2d7f
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-06-28 20:05:10 +0300

    Add files via upload

---
+++ Exams/Exam - 19.02.2022/PawnWars.java
@@ -0,0 +1,74 @@
+package Examp.exam19_02_2022;
+
+import java.util.Scanner;
+
+public class PawnWars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[][] matrix = new String[8][8];
+        int rowBlack = -1;
+        int colBlack = -1;
+        int rowWhite = -1;
+        int colWhite = -1;
+
+        for (int row = 0; row < 8; row++) {
+            String input = scanner.nextLine();
+            for (int col = 0; col < 8; col++) {
+                matrix[row][col] = String.valueOf(input.charAt(col));
+                if (input.charAt(col) == 'w') {
+                    rowWhite = row;
+                    colWhite = col;
+                }
+                if (input.charAt(col) == 'b') {
+                    rowBlack = row;
+                    colBlack = col;
+                }
+            }
+        }
+        while (true) {
+            if (isCapture(rowWhite - 1, colWhite - 1, "b", matrix)) {
+                break;
+            }
+            if (isCapture(rowWhite - 1, colWhite + 1, "b", matrix)) {
+                break;
+            }
+            if (rowWhite - 1 == 0) {
+                char col = (char) ('a' + colWhite);
+                System.out.println("Game over! White pawn is promoted to a queen at " + col + "8.");
+                break;
+            }
+
+            matrix[rowWhite][colWhite] = "-";
+            rowWhite--;
+            matrix[rowWhite][colWhite] = "w";
+
+            if (isCapture(rowBlack + 1, colBlack - 1, "w", matrix)) {
+                break;
+            }
+            if (isCapture(rowBlack + 1, colBlack + 1, "w", matrix)) {
+                break;
+            }
+            if (rowBlack + 1 == 7) {
+                char col = (char) ('a' + colBlack);
+                System.out.println("Game over! Black pawn is promoted to a queen at " + col + "1.");
+                break;
+            }
+
+            matrix[rowBlack][colBlack] = "-";
+            rowBlack++;
+            matrix[rowBlack][colBlack] = "b";
+        }
+    }
+
+    private static boolean isCapture(int row, int col, String pawn, String[][] matrix) {
+        if (row >= 0 && row <= 7 && col >= 0 && col <= 7 && matrix[row][col].equals(pawn)) {
+            String captureColour = pawn.equals("b") ? "White" : "Black";
+            char colCapture = (char) ('a' + col);
+            System.out.println("Game over! " + captureColour + " capture on " + colCapture + (8 - row) + ".");
+            return true;
+        }
+        return false;
+    }
+}
+
